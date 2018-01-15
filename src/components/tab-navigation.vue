4592424b31f7d71893b8e681caee425331f39840
unknown <paadams@nmu.edu>
2018-01-14 21:24:48 -0500

    remove unnecessary q-layout tags

--- src/components/tab-navigation.vue
+++ src/components/tab-navigation.vue
@@ -1,23 +1,21 @@
 <template>
-  <q-layout ref="layout">
-     <q-tabs slot="navigation">
-       <q-route-tab
-          slot="title"
-          to="/home"
-          label="Home"
-        />
-        <q-route-tab
-          slot="title"
-          to="/the-select-menus"
-          label="Browse locations"
-        />
-        <q-route-tab
-          slot="title"
-          to="/the-monitor-keys"
-          label="Monitor keys"
-        />
-     </q-tabs>
-  </q-layout>
+    <q-tabs slot="navigation">
+      <q-route-tab
+        slot="title"
+        to="/home"
+        label="Home"
+      />
+      <q-route-tab
+        slot="title"
+        to="/the-select-menus"
+        label="Browse locations"
+      />
+      <q-route-tab
+        slot="title"
+        to="/the-monitor-keys"
+        label="Monitor keys"
+      />
+    </q-tabs>
 </template>
 <script>
 import { QLayout, QTabs, QRouteTab } from "quasar";
--- src/components/the-select-menus.vue
+++ src/components/the-select-menus.vue
@@ -1,21 +1,20 @@
 <template>
-  <q-layout>
-     <q-select
-        v-model="countrySelect"
-        :options="supportedCountries"
-     />
-     <q-select
-        v-if="supportedStates.length"
-        v-model="stateSelect"
-        :options="supportedStates"
+  <div>
+      <q-select
+          v-model="countrySelect"
+          :options="supportedCountries"
       />
       <q-select
-        v-if="supportedCities.length"
-        v-model="citySelect"
-        :options="supportedCities"
-      />
-   
-  </q-layout>
+          v-if="supportedStates.length"
+          v-model="stateSelect"
+          :options="supportedStates"
+        />
+        <q-select
+          v-if="supportedCities.length"
+          v-model="citySelect"
+          :options="supportedCities"
+        />
+  </div>
 </template>
 <script>
 import { QLayout, QSelect } from "quasar";
